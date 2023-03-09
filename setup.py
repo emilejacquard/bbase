0dc1f5647f99a339356cce21d69cce58834d3a27
Emile Jacquard <jacquard.emile@gmail.com>
2023-03-09 23:05:45 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
+import os
+import re
 
+# Get version string from module
+init_path = os.path.join(os.path.dirname(__file__), "bbase/__init__.py")
+with open(init_path, "r", encoding="utf8") as f:
+    version = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M).group(1)
 setuptools.setup(
     name='bbase',
     install_requires=['numpy'],
