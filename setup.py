f7e86d3811a2ed65a7ca7cd32d6de7d2f175f8de
Emile Jacquard <jacquard.emile@gmail.com>
2023-03-09 22:57:46 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setuptools.setup(
     name='bbase',
+    install_requires=['numpy']
     version='0.0.1',
     author='Emile Jacquard',
     author_email='emile.jacquard@maths.ox.ac.uk',
