10026e9f1ff3310ebe17cbfa08ddba409f135e67
Emile Jacquard <jacquard.emile@gmail.com>
2023-03-09 17:42:11 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='bbase',
+    version='0.0.1',
+    author='Emile Jacquard',
+    author_email='emile.jacquard@maths.ox.ac.uk',
+    url='https://github.com/emilejacquard/bbase',
+    packages=['bbase']
+)
