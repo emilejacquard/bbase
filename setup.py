c4094a7b95dcdf193c198a756296f76d9306f155
Emile Jacquard <jacquard.emile@gmail.com>
2023-03-09 23:19:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
-from setuptools import setup
-import os
-import re
-
-# Get version string from module
-init_path = os.path.join(os.path.dirname(__file__), "bbase/__init__.py")
-with open(init_path, "r", encoding="utf8") as f:
-    version = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M).group(1)
+from setuptools import setup, find_packages
 setuptools.setup(
     name='bbase',
     install_requires=['numpy'],
     version='0.0.1',
     author='Emile Jacquard',
     author_email='emile.jacquard@maths.ox.ac.uk',
+    packages=find_packages(),
     url='https://github.com/emilejacquard/bbase',
     packages=['bbase']
 )
