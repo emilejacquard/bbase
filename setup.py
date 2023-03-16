292af3207188c6cf0840e9abe028571e988546f6
Emile Jacquard <jacquard.emile@gmail.com>
2023-03-16 23:32:43 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from bbase import __version__
 
 setuptools.setup(
     name='bbase',
-    install_requires=['numpy>=1.24.2'],
-    python_requires=">=3.10",
     version=__version__,
     author='Emile Jacquard',
     author_email='emile.jacquard@maths.ox.ac.uk',
