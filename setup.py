158a46c6aec45348b712007bd3dd6bd61232a1ca
Emile Jacquard <jacquard.emile@gmail.com>
2023-04-06 12:23:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     name='bbase',
     version=__version__,
     author='Emile Jacquard',
+    license='MIT',
     install_requires=['numpy>=1.24.2'],
     python_requires=">=3.8.0",
     author_email='emile.jacquard@maths.ox.ac.uk',
